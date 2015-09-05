1641f8284c1d8f7d3d54e4dc0e3c76a69bc6a949
charles-peter <charliepeter2011@yahoo.com>
2015-09-05 18:11:59 -0400

    TargetHUD
    Started the set up for getting the target arrow to show above your
    currently targeted enemy.

--- InifiniteSpace/Assets/Scripts/Player_HUD.cs
+++ InifiniteSpace/Assets/Scripts/Player_HUD.cs
@@ -8,6 +8,7 @@ public class Player_HUD : MonoBehaviour
 	//Health Bar information
 	float m_startHealth;
 	public UnityEngine.UI.Slider m_HealthBar;
+	public Transform m_Target;
 
 	// Use this for initialization
 	void Start () 
@@ -23,6 +24,13 @@ public class Player_HUD : MonoBehaviour
 		{
 			float healthRat = m_Player.Health / m_startHealth;
 			m_HealthBar.value = m_HealthBar.maxValue * healthRat;
+
+			if(m_Player.m_Target != null)
+			{
+				m_Target = m_Player.m_Target.transform;
+				Vector3 toCanvas = gameObject.transform.position - m_Player.m_Target.transform.position;
+				m_Target.Rotate(toCanvas);
+			}
 		}
 	}
 }

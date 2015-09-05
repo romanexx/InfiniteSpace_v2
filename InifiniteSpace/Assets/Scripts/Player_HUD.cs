2ccf23027d5f32cb92cb8128ffc3f98767e8422a
charles-peter <charliepeter2011@yahoo.com>
2015-09-05 16:47:39 -0400

    Health Bar
    Added a health bar. This should move once you get the take damage stuff
    working.

---
+++ InifiniteSpace/Assets/Scripts/Player_HUD.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_HUD : MonoBehaviour 
+{
+	SpaceShip_Controller m_Player = null;
+
+	//Health Bar information
+	float m_startHealth;
+	public UnityEngine.UI.Slider m_HealthBar;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Player = FindObjectOfType<SpaceShip_Controller>();
+		m_startHealth = m_Player.Health;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (m_Player != null) 
+		{
+			float healthRat = m_Player.Health / m_startHealth;
+			m_HealthBar.value = m_HealthBar.maxValue * healthRat;
+		}
+	}
+}

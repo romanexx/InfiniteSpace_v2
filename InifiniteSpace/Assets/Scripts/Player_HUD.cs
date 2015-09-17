fdec734d84ab1f36141dd4150a1bb74cc842d9bd
charles-peter <charliepeter2011@yahoo.com>
2015-09-17 17:01:57 -0400

    Added a life counter
    Lifes now show up on the bottom left of the hud.

--- InifiniteSpace/Assets/Scripts/Player_HUD.cs
+++ InifiniteSpace/Assets/Scripts/Player_HUD.cs
@@ -7,16 +7,21 @@ public class Player_HUD : MonoBehaviour
 	SpaceShip_Controller m_Player = null;
 	GameController m_Game = null;
 
-	//Health Bar information
+	//Click and drag object refrences
 	public UnityEngine.UI.Slider m_HealthBar;
-
 	public Text m_Score;
+	public RectTransform m_Lives;
+
+	//Life Images control variables.
+	float m_ImgWidth = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
 		m_Player = FindObjectOfType<SpaceShip_Controller>();
 		m_Game = FindObjectOfType<GameController>();
+		m_Lives.sizeDelta = new Vector2 (m_Lives.sizeDelta.x * m_Game.M_Lives, m_Lives.sizeDelta.y);
+		m_ImgWidth = m_Lives.sizeDelta.x / (float)m_Game.M_Lives;
 	}
 	
 	// Update is called once per frame
@@ -27,8 +32,7 @@ public class Player_HUD : MonoBehaviour
 			float healthRat = m_Player.M_health / m_Player.MaxHealth;
 			m_HealthBar.value = m_HealthBar.maxValue * healthRat;
 			m_Score.text = m_Game.M_Score.ToString();
-
-
+			m_Lives.sizeDelta = new Vector2(m_ImgWidth * (float)m_Game.M_Lives, m_Lives.sizeDelta.y);
 		}
 	}
 }

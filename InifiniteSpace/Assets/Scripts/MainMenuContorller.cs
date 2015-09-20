dedee62e9e5d67bba2aa31b42e2073632fcf1e12
charles-peter <charliepeter2011@yahoo.com>
2015-09-20 14:48:28 -0400

    Added Credits
    Theres a credit button in the Main menu the have credits for us, the
    models, and other things we used.

--- InifiniteSpace/Assets/Scripts/MainMenuContorller.cs
+++ InifiniteSpace/Assets/Scripts/MainMenuContorller.cs
@@ -1,12 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenuContorller : MonoBehaviour 
 {
+	Button[] m_Buttons; 
+
+	public Text m_Credits; 
+	float m_CreYPos = 0.0f; //Default Position for the credits.
+
+	void Start()
+	{
+		m_Credits.enabled = false;
+		m_CreYPos = m_Credits.rectTransform.position.y;
+		m_Buttons = FindObjectsOfType<Button>();
+	}
+
+	void Update()
+	{
+		if ((m_Credits.rectTransform.position.y > 100.0f || Input.GetKey(KeyCode.Return) || Input.GetKey("escape")) && m_Credits.enabled == true) 
+		{
+			m_Credits.enabled = false; 
+			m_Credits.rectTransform.position = new Vector3(m_Credits.rectTransform.position.x, m_CreYPos, m_Credits.rectTransform.position.z);
+			for (int i = 0; i < m_Buttons.Length; ++i) 
+			{
+				m_Buttons[i].gameObject.SetActive(true);
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (m_Credits.enabled == true) 
+		{
+			m_Credits.rectTransform.position = new Vector3(m_Credits.rectTransform.position.x, m_Credits.rectTransform.position.y + 0.25f, m_Credits.rectTransform.position.z);
+		}
+	}
 
 	public void StartButton()
 	{
 		Application.LoadLevel ("Level_1");
+	}
+
+	public void Credits()
+	{
+		m_Credits.enabled = true;
+		for (int i = 0; i < m_Buttons.Length; ++i) 
+		{
+			m_Buttons[i].gameObject.SetActive(false);
+		}
 	}
 
 	public void ExitButton()

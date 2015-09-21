94b9fe00a47f52b33bb118b7b9685cc7b61a23ec
Roman Taran <Roman.v.taran@gmail.com>
2015-09-21 13:19:56 -0400

    Added more environment stuff to level 1
    making it pretty

---
+++ InifiniteSpace/Assets/TextureScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroll : MonoBehaviour {
+
+
+	public Vector2 texScroll;
+	MeshRenderer render;
+	// Use this for initialization
+	void Start () 
+	{
+		render = GetComponent<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//texScroll += texScroll  * Time.deltaTime;
+		texScroll.x = Time.time * 0.1f;
+		texScroll.y = Time.time * 0.1f;
+		render.material.mainTextureOffset = texScroll;
+	}
+}
